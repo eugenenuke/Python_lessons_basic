a00b4cb88de130346d9434d21b89d641af6ae65e
Evgeniy Yanyuk <eugenenuke@gmail.com>
2018-03-03 10:47:02 +0300

    hw01_hard.py is done

--- lesson01/home_work/hw01_hard.py
+++ lesson01/home_work/hw01_hard.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env python3
 __author__ = 'Ваши Ф.И.О.'
 
 # Задание-1:
@@ -14,3 +14,31 @@ __author__ = 'Ваши Ф.И.О.'
 # если точно известно, что её значение не изменялось?
 
 # Подсказка: это значение точно есть ;)
+
+a = float('Inf')
+
+print('a =', a)
+print('a == a*2:', a == a*2)
+print('a == a**2:', a == a**2)
+print('a > 999999:', a > 999999)
+
+
+print()
+
+
+# Вариант 2
+# hack the int
+from random import randint
+
+class mad_int(int):
+    def __eq__(self, x):
+        return True
+    def __gt__(self, x):
+        return True
+
+a = mad_int(randint(0, 1000000))
+
+print('a =', a)
+print('a == a*2:', a == a*2)
+print('a == a**2:', a == a**2)
+print('a > 999999:', a > 999999)
