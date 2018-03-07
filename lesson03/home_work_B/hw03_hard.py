0cdc460df9c3300de1d04bedbd36bf089b20207f
Evgeniy Yanyuk <eugenenuke@gmail.com>
2018-03-07 15:36:18 +0300

    hw03_hard.py is competed

--- lesson03/home_work_B/hw03_hard.py
+++ lesson03/home_work_B/hw03_hard.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # Задание - 1
 # Давайте опишем пару сущностей player и enemy через словарь,
 # который будет иметь ключи и значения:
@@ -11,6 +12,26 @@
 # health от атакуемого. Функция должна сама работать с словарями и изменять их значения.
 
 
+from random import choice
+
+
+def attack(attacker, target):
+    target['health'] -= attacker['damage']
+
+
+print('Задача №1')
+
+player = {'name': input("Введите своё имя: "), 'health': 80, 'damage': 35}
+enemy = {'name': input("Введите имя врага: "), 'health': 100, 'damage': 25}
+
+print(player, enemy, sep='\n')
+
+print('Атака!')
+attack(player, enemy)
+
+print(player, enemy, sep='\n')
+
+
 # Задание - 2
 # Давайте усложним предыдущее задание, измените сущности, добавив новый параметр - armor = 1.2
 # Теперь надо добавить функцию, которая будет вычислять и возвращать полученный урон по формуле damage / armor
@@ -22,3 +43,55 @@
 # после чего происходит запуск игровой сессии, где сущностям поочередно наносится урон,
 # пока у одного из них health не станет меньше или равен 0.
 # После чего на экран должно быть выведено имя победителя, и количество оставшихся единиц здоровья.
+
+def get_damage(damage, armor):
+    return damage / armor
+
+
+def attack2(attacker, target):
+    target['health'] -= get_damage(attacker['damage'], target['armor'])
+
+
+def dict2str(d):
+    return ['{}:{}\n'.format(x, y) for x, y in d.items()]
+    # return list(map(lambda x: '{}:{}\n'.format(x[0], x[1]), d.items()))
+
+
+def str2dict(str):
+    d = dict()
+    for s in str:
+        key, value = s.rstrip().split(':')
+        if key != 'name':
+            value = float(value)
+        d[key] = value
+    return d
+
+
+print('\nЗадача №2')
+
+player = {'name': player['name'], 'health': 80, 'damage': 35, 'armor': 1.2}
+enemy = {'name': enemy['name'], 'health': 100, 'damage': 25, 'armor': 1.2}
+
+for entity in (player, enemy):
+    with open(entity['name'] + '.txt', 'w') as ef:
+        ef.writelines(dict2str(entity))
+
+player['armor'] = 0
+with open(player['name'] + '.txt', 'r') as pf:
+    player = str2dict(pf.readlines())
+
+enemy['armor'] = 0
+with open(enemy['name'] + '.txt', 'r') as ef:
+    enemy = str2dict(ef.readlines())
+
+print('Начинаем игру!')
+print(player['name'], '\t', enemy['name'])
+print(round(player['health'], 2), '\t', round(enemy['health'], 2))
+
+while 0 < player['health'] and 0 < enemy['health']:
+    if choice([True, False]):
+        attack2(player, enemy)
+    else:
+        attack2(enemy, player)
+    print(round(player['health'], 2), '\t', round(enemy['health'], 2))
+print('Победил,', (enemy['name'], player['name'])[player['health'] > 0])
