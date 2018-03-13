daef4fa9b820ec63fcec2a519b9461f17ef675b0
Evgeniy Yanyuk <eugenenuke@gmail.com>
2018-03-13 12:52:07 +0300

    hw05_easy is done

--- lesson05/home_work/hw05_easy.py
+++ lesson05/home_work/hw05_easy.py
@@ -9,38 +9,69 @@ import shutil
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
 
-print('Создаём директории dir_1 - dir_9...')
-for i in range(1, 10):
-    dir = 'dir_' + str(i)
+def rmdir(rdir):
     try:
-        os.mkdir(dir)
+        #os.rmdir(rdir)
+        shutil.rmtree(rdir)
     except Exception as e:
-        print('Ошибка при создании директории {}: {}'.format(dir, e))
+        print('Ошибка при удалении директории {}: {}'.format(rdir, e))
+        return False
+    return True
 
-print('Директории созданы, удаляем их...')
-for i in range(1, 10):
-    dir = 'dir_' + str(i)
+
+def mkdir(mdir):
     try:
-        os.rmdir(dir)
-        # shutil.rmtree(dir)
+        os.mkdir(mdir)
     except Exception as e:
-        print('Ошибка при удалении директории {}: {}'.format(dir, e))
-print('Директории удалены.\n')
+        print('Ошибка при создании директории {}: {}'.format(mdir, e))
+        return False
+    return True
+
 
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
-print('Список папок текущей директории:')
-answer = os.listdir()
-for item in answer:
-    if os.path.isdir(item):
-        print(item)
+def lscwd(ftype='a'):
+    '''
+    ftype:
+        a - All
+        f - files
+        d - dirs
+    '''
+    contents = os.listdir()
+    for item in contents:
+        if ftype == 'a' or ftype == 'd' and os.path.isdir(item) or \
+           ftype == 'f' and os.path.isfile(item):
+            print(item)
+
 
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
-print('Создаём копию файла: cp {} {}'.format(sys.argv[0], sys.argv[0] + '.copy'))
-try:
-    res = shutil.copy(sys.argv[0], sys.argv[0] + '.copy')
-except Exception as e:
-    print('Ошибка при копировании:', e)
-else:
-    print('Файл успешно скопирован в', res)
+def f_copy(source, target):
+    try:
+        res = shutil.copy(source, target)
+    except Exception as e:
+        print('Ошибка при копировании:', e)
+    else:
+        print('Файл успешно скопирован в', res)
+
+
+if __name__ == '__main__':
+    # Задача-1:
+    print('Создаём директории dir_1 - dir_9...')
+    for i in range(1, 10):
+        mydir = 'dir_' + str(i)
+        mkdir(mydir)
+
+    print('Директории созданы, удаляем их...')
+    for i in range(1, 10):
+        mydir = 'dir_' + str(i)
+        rmdir(mydir)
+    print('Директории удалены.\n')
+
+    # Задача-2:
+    print('Список папок текущей директории:')
+    lscwd(ftype='d')
+
+    # Задача-3:
+    print('Создаём копию файла: cp {} {}'.format(sys.argv[0], sys.argv[0] + '.copy'))
+    f_copy(sys.argv[0], sys.argv[0] + '.copy')
