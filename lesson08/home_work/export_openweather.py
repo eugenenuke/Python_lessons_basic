0b054a2c000c06d0fe1b4af0a3c4c2c0ad36e76f
Evgeniy Yanyuk <eugenenuke@gmail.com>
2018-03-25 17:15:54 +0300

    export_openweather.py is done

--- lesson08/home_work/export_openweather.py
+++ lesson08/home_work/export_openweather.py
@@ -23,3 +23,80 @@
 
 import csv
 import json
+import sqlite3
+import sys
+import getopt
+
+DB_FILE = 'temp_data.db'
+
+
+def export_json(data, filename='out.json'):
+    try:
+        with open(filename, 'w') as j_out:
+            j_out.writelines('\n'.join([json.dumps(row) for row in data]))
+    except OSError:
+        print('Ошибка записи в файл')
+        sys.exit(1)
+
+def export_csv(data, filename='out.csv'):
+    try:
+        with open(filename, 'w') as csv_out:
+            fields = ['city_id', 'city_name', 'date', 'temp', 'weather_id']
+            csv_writer = csv.DictWriter(csv_out, fields)
+            csv_writer.writeheader()
+            for row in data:
+                csv_writer.writerow(row)
+    except OSError:
+        print('Ошибка записи в файл')
+        sys.exit(1)
+
+def export_html(data, filename='out.html'):
+    print('Экспорт в html не реализован.')
+
+def check_opts(opts):
+    for opt in opts:
+        if opt[0] in ['--json', '--csv', '--html']:
+            return True
+    return False
+
+def usage():
+    return 'Usage: {} --json | --csv | --html <filename> [city_name]'.format(sys.argv[0])
+
+
+data = []
+try:
+    opts, args = getopt.getopt(sys.argv[1:], '', ['json=', 'csv=', 'html='])
+except getopt.GetoptError:
+    print(usage())
+    sys.exit(1)
+
+if len(args) > 1 or not check_opts(opts):
+    print(usage())
+    sys.exit(1)
+
+try:
+    with sqlite3.connect(DB_FILE) as conn:
+        cur = conn.cursor()
+        if args:
+            cur.execute("SELECT * FROM temp_data WHERE city_name=?", (args[0], ))
+        else:
+            cur.execute("SELECT * FROM temp_data")
+        for row in cur:
+            data.append({
+                'city_id': row[0],
+                'city_name': row[1],
+                'date': row[2],
+                'temp': row[3],
+                'weather_id': row[4]
+                })
+except Exception as e:
+    print(e)
+
+for opt in opts:
+    if opt[0] == '--json':
+        export_json(data, opt[1])
+    if opt[0] == '--csv':
+        export_csv(data, opt[1])
+    if opt[0] == '--html':
+        export_html(data, opt[1])
+
